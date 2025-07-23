dc51256fad77439f676dacdc903329bf7f3fc3d5
buskod21 <bkodaolu@uoguelph.ca>
2025-07-23 12:59:51 +0000

    updated documentation

---
+++ docs/zoom.js
@@ -0,0 +1,8 @@
+<script src="https://cdn.jsdelivr.net/npm/medium-zoom@1.0.6/dist/medium-zoom.min.js"></script>
+<script>
+  mediumZoom('img', {
+    margin: 24,
+    background: '#000',
+    scrollOffset: 0,
+  })
+</script>
